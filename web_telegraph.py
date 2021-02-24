9634a32adedbcf586ffd4b8fc0a579a84d369866
marinaepitropaki <marinaepitropaki@gmail.com>
2021-02-25 00:44:59 +0200

    telescope hover fix

--- web_telegraph.py
+++ web_telegraph.py
@@ -263,14 +263,14 @@ def update_figure(n_intervals, data, telescope_position):
                 x = x + 12
             if x > 12 :
                 x = x - 12
-        hover_tele_hours, hover_tele_deg = deg_to_hex(telescope_position_hours, 
-                                                    telescope_position_degrees)
+        # hover_tele_hours, hover_tele_deg = deg_to_hex(telescope_position_hours, 
+        #                                             telescope_position_degrees)
 
         fig.add_trace(go.Scatter(x=telescope_position_hours, 
                                  y=[telescope_position['degrees']], 
-                                 hovertemplate=[
-                                f'HA:{str(hover_tele_hours)} DEC:{str(hover_tele_deg)}'
-                                ],
+                                #  hovertemplate=[
+                                # f'HA:{str(hover_tele_hours)} DEC:{str(hover_tele_deg)}'
+                                # ],
                                 mode="markers+text", text=['Telescope']),
                                 row=1, col=1)
 
